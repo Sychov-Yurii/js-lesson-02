5909f4545e98f5bc7860b9aaff0d04fe33671e4e
Sychov-Yurii <sychov.yurii@outlook.com>
2023-05-19 21:01:02 +0200

    1

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // ================= work 1 ================= //
 
+function checkAge(age) {
+    if (age>=20) { // Change this line
+      return "You are an adult";
+    }
+  
+    return "You are a minor";
+  }
+
+// ================= work 2 ================= //
+
