1d965191c72ef64625b981353706d28f363db6a2
Sychov-Yurii <sychov.yurii@outlook.com>
2023-05-20 08:44:05 +0200

    11

--- script.js
+++ script.js
@@ -10,26 +10,121 @@
 
 // ================= work 2 ================= //
 
-
+// function checkPassword(password) {
+//     const ADMIN_PASSWORD = "jqueryismyjam";
+//     // Change code below this line
+  
+  
+//     if (password === ADMIN_PASSWORD) {
+//       return "Welcome!";
+//     } 
+    
+    
+//       return "Access denied, wrong password!";
+//     }
 
 // ================= work 3 ================= //
 
-
+// function checkStorage(available, ordered) {
+//     // Change code below this line
+  
+//     if (ordered === 0) {
+//       return "Your order is empty!";
+//     } if (ordered > available) {
+//       return "Your order is too large, not enough goods in stock!";
+//     } 
+//       return "The order is accepted, our manager will contact you";
+    
+//     // Change code above this line
+//   }  
 
 // ================= work 4 ================= //
 
-
-
+// // Change code below this line
+// const fruits = ["apple", "plum", "pear", "orange"]
 
 // ================= work 5 ================= //
 
+// const fruits = ["apple", "plum", "pear", "orange"];
 
-
+// // Change code below this line
+// const firstElement = fruits[0];
+// console.log(firstElement)
+// const secondElement = fruits[1];
+// const lastElement = fruits[3];
 
 // ================= work 6 ================= //
 
+// const fruits = ["apple", "plum", "pear", "orange"];
 
+// // Write your code under this line
 
+// fruits [1] = "peach";
+// fruits [3] = "banana";
 
 // ================= work 7 ================= //
 
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// // Change code below this line
+// const fruitsArrayLength = (fruits.length)
+
+// ================= work 8 ================= //
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// // Change code below this line
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+// ================= work 9 ================= //
+
+// function getExtremeElements(array) {
+//     // Change code below this line
+//   let firstElements = array [0];
+//   let lastElementIndex = array[array.length - 1]; 
+//   let lastElement = [firstElements, lastElementIndex]; 
+//   return lastElement
+//     // Change code above this line
+//   }
+
+// ================= work 10 ================= //
+
+// function splitMessage(message, delimiter) {
+//     let words;
+//     // Change code below this line
+//   words = message.split(delimiter)
+//     // Change code above this line
+//     return words;
+//   }  
+
+// ================= work 11 ================= //
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//     // Change code below this line
+//    let long;
+//    let longer;
+//    let totalCost;
+//    long = message.split(" ");
+//    longer = long.length;
+//    totalCost = pricePerWord * longer;
+ 
+//    return totalCost
+//     // Change code above this line
+//  }
+
+// ================= work 12 ================= //
+
+
+
+// ================= work 13 ================= //
+
+
+
+
+// ================= work 14 ================= //
+
+
+
+
+// ================= work 15 ================= //
