ffae1939085f853a4533ca2a43a6976f633c6a9c
Sychov-Yurii <sychov.yurii@outlook.com>
2023-05-30 19:03:25 +0200

    02

--- script.js
+++ script.js
@@ -136,6 +136,15 @@
 //     // Change code above this line
 //   }
 
+// another variant to made 13s homework
+
+
+// function slugify(title) {
+//   // Change code below this line
+// return title.split(" ").join("-").toLowerCase()
+//   // Change code above this line
+// }
+
 // ================= work 14 ================= //
 
 // const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
@@ -376,3 +385,50 @@
 //   // Change code above this line
 // }
 
+
+const atTheOldToad = {
+    potions: [
+      { name: "Speed potion", price: 460 },
+      { name: "Dragon breath", price: 780 },
+      { name: "Stone skin", price: 520 },
+    ],
+    // Change code below this line
+    getPotions() {
+      return this.potions;
+    },
+    addPotion(newPotion) {
+      for (const potion of this.potions) {
+      if (potion.name === newPotion.name) {
+        return `Error! Potion ${newPotion.name} is already in your inventory!`;
+      }  
+      }
+      this.potions.push(newPotion);
+    },
+    removePotion(potionName) {
+      let potionIndex = -1;
+      for (const potion of this.potions) {
+         if (potion.name === potionName) {
+           potionIndex = this.potions.indexOf(potion)
+        ;
+      }
+      }   
+  
+      this.potions.splice(potionIndex, 1);
+    },
+    updatePotionName(oldName, newName) {
+      let potionIndex = 0;
+      for (const potion of this.potions) {
+        if (potion.name === oldName) {
+          potionIndex = this.potions.indexOf(potion);
+          potion.name = newName;
+        }
+        if (potionIndex === -1) {
+          return `Potion ${oldName} is not in inventory!`;
+        }
+      }
+  
+  
+    // Change code above this line
+  }
+}
+  
